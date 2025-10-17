2f330433b2803746d23e031c3335390321e0ac1d
colindebin <colindeb1n2@gmail.com>
2025-10-16 21:00:47 -0400

    Edited

--- exploratory_and_misc/pulse_width_during_B_mod.py
+++ exploratory_and_misc/pulse_width_during_B_mod.py
@@ -4,104 +4,48 @@ sys.path.insert(1, '../')  # Add parent directory to path for custom libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.gridspec as gridspec
+
+from scipy.optimize import curve_fit
 
 # Custom libraries
 from analysis.library import styles, plt_settings, colors
-from analysis.breit_rabi import FreqMHz
+from analysis.breit_rabi import FreqMHz, Ehf
 
+
+plt_settings['font.size'] = 16
+plt_settings["legend.fontsize"] = 12
 plt.rcParams.update(plt_settings)
 
 def f97(B):
     """Frequency of the 9/2,-9/2 to 9/2,-7/2 transition in kHz at field B (in Gauss)."""
     return FreqMHz(B, 9/2, -9/2, 9/2, -7/2)*1e3  # in kHz
 
+
 def field_modulation(t, B0, Bmod, fmod):
-    """
-    Calculate the magnetic field with modulation.
-
-    Parameters:
-    B0 : float
-        The static magnetic field in Tesla.
-    Bmod : float
-        The amplitude of the modulation in Tesla.
-    fmod : float
-        The frequency of the modulation in Hz.
-    t : array-like
-        Time array in seconds.
-
-    Returns:
-    B_total : array-like
-        The total magnetic field at each time point.
-    """
+    """Calculate the magnetic field with modulation."""
     return B0 + Bmod * np.sin(2 * np.pi * fmod * t)
-
-def pulse_times(tmin, tmax, pulse_width, num_pulses=5):
-    """
-    Generate center times for a series of pulses.
-
-    Parameters:
-    tmin : float
-        Minimum time in ms.
-    tmax : float
-        Maximum time in ms.
-    pulse_width : float
-        Width of each pulse in ms.
-    num_pulses : int
-        Number of pulses.
-
-    Returns:
-    pulse_times : array-like
-        Center times of the pulses in ms.
-    """
-    return np.linspace(tmin + pulse_width / 2, tmax - pulse_width / 2, num_pulses)
 
 
 def square_pulse(t, pulse_width, t0):
-    """
-    Generate a square pulse.
-
-    Parameters:
-    t : array-like
-        Time array in ms.
-    pulse_width : float
-        Width of the pulse in ms.
-    t0 : float
-        Center time of the pulse in ms.
-
-    Returns:
-    pulse : array-like
-        The amplitude of the pulse at each time point.
-    """
+    """Generate a square pulse."""
     return np.where((t >= t0 - pulse_width / 2) & (t <= t0 + pulse_width / 2), 1.0, 0.0)
 
-def sinc2(detuning, pulse_width):
-    """Parameters:
-    detuning : array-like
-        Frequency detuning array in kHz.
-    pulse_width : float
-        Width of the pulse in ms.
 
-    Returns:
-    sinc2_values : array-like
-        The sinc^2 values at each frequency detuning.
-    """
+def sinc2(detuning, pulse_width):
     return (np.sinc(detuning * pulse_width))**2
 
-def freq_shift(B, B0=202.14):
-    """
-    Convert magnetic field to frequency shift in kHz.
 
-    Parameters:
-    B : array-like
-        Magnetic field in Gauss
-    B0 : float
-        Reference field in Guass, default is 202.14 G.
+def freq_shift(dB, B0=202.14):
+    """Convert magnetic field to frequency shift in kHz."""
+    return (f97(dB + B0) - f97(B0))  # in kHz
 
-    Returns:
-    freq : array-like
-        Frequency shift in kHz.
-    """
-    return (f97(B) - f97(B0))  # in kHz
+
+def B_shift(df, B0=202.14):
+    """Convert frequency shift in kHz to magnetic field in Gauss."""
+    dB = 0.1  # Small field step in Gauss
+    return df * dB / (f97(B0 + dB) - f97(B0 - dB))  # in Gauss
+
 
 def response(detuning, t0, Bt, pulse_width, num=1000):
     """
@@ -124,80 +68,124 @@ def response(detuning, t0, Bt, pulse_width, num=1000):
     """
     t = np.linspace(t0 - pulse_width/2, t0 + pulse_width/2, num)  # Time array around pulse center
     detunings = detuning*np.ones(num) - freq_shift(Bt(t))
-    return np.trapezoid(sinc2(detunings, pulse_width), dx=t[1]-t[0])/num
+    return np.trapezoid(sinc2(detunings, pulse_width), dx=t[1]-t[0])
+
+
+def percentile_range(x_vals, y_vals, percentiles=(40, 60)):
+    """Calculate the range between the given percentiles."""
+    lower, upper = percentiles
+    return np.percentile(x_vals, upper, method='inverted_cdf', weights=y_vals) \
+            - np.percentile(x_vals, lower, method='inverted_cdf', weights=y_vals)
+
+def fit_to_sinc2(x, y):
+    """Fit data to a sinc^2 function."""
+    guess_freq_width = 1/0.1  # 1/ms
+    popt, _ = curve_fit(lambda x, x0, pw: sinc2(x-x0, 1/pw), x, y, p0=[0, guess_freq_width])
+    return popt[0]*2, popt[1]
+
 
 ###
 ### Parameters
 ###
 B0 = 0  # arb.
-Bmod = 1  # arb.
+Bmod = 0.1  # arb.
 fmod = 10  # kHz
 tmin = 0  # ms
 tmax = 0.2  # ms
 t = np.linspace(tmin, tmax, 1000)  # from 0 to 0.2 ms
-B_total = field_modulation(t, B0, Bmod, fmod)
-
-pulse_widths = [0.02, 0.04, 0.08]  # ms
+dB = field_modulation(t, B0, Bmod, fmod)
 hatch_patterns = ['/', '\\', '|']
 delta = 0.001  # Small offset to avoid overlap in hatch patterns
 
+def pulse_times(tmin, tmax, num_pulses=5):
+    """Generate center times for a series of pulses."""
+    return np.linspace(tmin + 0.1/fmod, tmax - 0.1/fmod, num_pulses)
 
-###
-### Plot response vs. detuning and pulse widths
-###
-fig, ax = plt.subplots(figsize=(6, 4))
-ax.set(xlabel='Detuning (kHz)', ylabel='Response (arb.)')
+pulse_widths = [0.01, 0.02, 0.04, 0.08, 0.1]  # ms
 
-pulse_centres = pulse_times(tmin, tmax, min(pulse_widths))  # Center times of pulses
+fit_B_mods = []
+for pulse_width in pulse_widths:
 
-for time, pulse_width, color in zip(pulse_centres, pulse_widths, colors):
-    detunings = np.linspace(-2/pulse_width, 2/pulse_width, 200)  # kHz
-    ax.plot(detunings, sinc2(detunings, pulse_width), '--', color=color,
-            label='static response')
-    
-    response_vals = [response(d, time, lambda t: field_modulation(t, B0, Bmod, fmod), pulse_width) for d in detunings]
-    ax.plot(detunings, response_vals, '-', color=color,
-            label='response at t={:.2f} ms'.format(time))
-    break
+    ###
+    ### Plot field response vs. time and pulse widths.
+    ###
+    fig = plt.figure(figsize=(12, 5))
+    gs = gridspec.GridSpec(2,2, figure=fig, height_ratios=[3,1])
 
-ax.legend()
-fig.tight_layout()
+    ### Plot response vs. detuning
+    ax0 = fig.add_subplot(gs[:, 0])
+    ax0.set(xlabel='Detuning (kHz)', ylabel='Normalized Response (arb.)')
+
+    pulse_centres = pulse_times(tmin, tmax, num_pulses=7)  # Center times of pulses.
+    pulse_amp = min(pulse_widths)/pulse_width  # Normalize pulse amplitude for visibility
+
+    patch_width = pulse_width - delta # Adjusted width for visibility
+
+    detunings = np.linspace(-2.5/pulse_width, 2.5/pulse_width, 2000)  # kHz
+    ax0.set(xlim=(detunings[0], detunings[-1]))
+    static_response = sinc2(detunings, pulse_width)
+    ax0.plot(detunings, static_response/np.max(static_response), '--', color='k',
+                label='static')
+
+    spectra_widths = []
+    B_responses = []
+    for time, color in zip(pulse_centres, colors):
+        dBt = lambda t: field_modulation(t, B0, Bmod, fmod)  # Bfield as function of time.
+
+        response_vals = np.array([response(d, time, dBt, pulse_width) for d in detunings])
+
+        normalized_response = response_vals/np.max(response_vals)
+        percentile_width = B_shift(percentile_range(detunings, normalized_response)/2)
+
+        shift, width = fit_to_sinc2(detunings, normalized_response)
+        B_responses.append(B_shift(shift))
+        spectra_widths.append(B_shift(width))
+
+        ax0.plot(detunings, normalized_response, '-', color=color,
+                    label='t={:.2f} ms'.format(time))
+    ax0.legend()
+
+    popt, pcov = curve_fit(lambda t, Bmod: Bmod*np.sin(2*np.pi*t*fmod), pulse_centres, B_responses, p0=[Bmod])
+    perr = np.sqrt(np.diag(pcov))
+    print("Fitted parameters: Bmod={:.4f}({:.0f})".format(popt[0], perr[0]*1e4))
+    fit_B_mods.append(popt[0])
+    fit_B_oscillation = lambda t: popt[0]*np.sin(2*np.pi*t*fmod)
+
+    ### Plot Field modulation and pulse sequence
+    ax1 = fig.add_subplot(gs[0, 1])
+    ax2 = fig.add_subplot(gs[1, 1])
+
+    ax1.set(xlim=(tmin, tmax), ylabel='B field change (arb.)', xticklabels=[])
+    ax2.set(xlim=(tmin, tmax), xlabel='Time (ms)')
+
+    # Plot the magnetic field modulation
+    ax1.plot(t, fit_B_oscillation(t), '-', label=r'fit $\delta B(t)$')
+    ax1.plot(t, dB, '--', label=r'$\delta B(t)$')
+
+    for i, time in enumerate(pulse_centres):
+        # dB_at_pulse = field_modulation(time, B0, Bmod, fmod)
+        
+        xerr = pulse_width/2
+        ax1.errorbar(time, B_responses[i], #yerr=spectra_widths[i], xerr=xerr, 
+                    fmt='o', color=colors[i], label='t={:.2f} ms'.format(time))
+
+        rect = patches.Rectangle((time - patch_width/2, 0), patch_width, pulse_amp, 
+                                linewidth=1, edgecolor=colors[i], facecolor=colors[i], 
+                                alpha=0.5, hatch=hatch_patterns[0])
+        ax2.add_patch(rect)
+
+    # ax1.legend()
+
+    fig.suptitle("Pulse width = {:.2f} ms, Modulation period = {:.2f} ms".format(pulse_width, 1/fmod))
+    fig.tight_layout()
+    plt.show()
+
+
+plt.figure(figsize=(6,4))
+plt.plot(np.array([0] + pulse_widths)*fmod, np.array([0.1] + fit_B_mods)/Bmod, 'o-')
+plt.xlabel('Pulse width/Period')
+plt.ylabel('Fit B amplitude/B amp')
+plt.tight_layout()
+# plt.title("Non-interacting, transfer during field modulation",)
 plt.show()
-
-###
-### Plot field response vs. time and pulse widths
-###
-fig, axs = plt.subplots(2, 1, figsize=(8, 6), height_ratios=[3, 1])
-
-axs[0].set_ylabel('Magnetic Field (arb.)')
-axs[1].set_ylabel('Pulse Amp (arb.)')
-
-for ax in axs:
-    ax.set(xlim=(tmin, tmax), xlabel='Time (ms)')
-
-# Plot the magnetic field modulation
-ax = axs[0]
-ax.plot(t, B_total, '--', label='B(t)')
-
-for pulse_width, color, hatch_pattern in zip(pulse_widths, colors, hatch_patterns):
-    pulse_centres = pulse_times(tmin, tmax, pulse_width) # Center times of pulses
-    B_at_pulses = field_modulation(pulse_centres, B0, Bmod, fmod)
-
-    axs[0].plot(pulse_centres, B_at_pulses, 'o', color=color, 
-                label='w={:.2f} ms'.format(pulse_width))
-
-    # Plot the rf pulses as rectangles
-    for pt in pulse_centres:
-        pulse_amp = min(pulse_widths)/pulse_width  # Normalize pulse amplitude for visibility
-        width = pulse_width - delta # Adjusted width for visibility
-        rect = patches.Rectangle((pt - width/2, 0), width, pulse_amp, 
-                                linewidth=1, edgecolor=color, facecolor=color, alpha=0.5,
-                                hatch=hatch_pattern)
-        axs[1].add_patch(rect)
-
-axs[0].legend()
-
-fig.tight_layout()
-plt.show()
-
 
