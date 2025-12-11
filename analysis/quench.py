33da8144a4cca817f3f38e70980148cd2939737e
uoft-chiplab <64617998+kpondaugusto@users.noreply.github.com>
2025-12-10 21:34:52 -0500

    Update quench.py

--- analysis/quench.py
+++ analysis/quench.py
@@ -5,13 +5,23 @@ This script analyzes the growth of the contact following an rf quench into unita
 
 # settings for directories, standard packages...
 from preamble import *
+from library import colors
 
-runs = ["2025-12-10_I", "2025-12-10_J"]
-defaults = {'freq':43.24, 'EF':18000, 'trf':1e-5, 'T':300} # default values for freq (MHz), EF (Hz), trf (s)
+runs = ["2025-12-10_I", # not shuffled
+        "2025-12-10_J",
+        "2025-12-10_Q"]
+EFs = [14500, 
+       14500,
+       11900] # Hz
+Ts = [369,
+      369,
+      195] # nK
+defaults = {'freq':43.24, 'trf':1e-5} # default values for freq (MHz), trf (s)
+
 
 fig, ax = plt.subplots(figsize=(4,3))
 ax.set(xlabel = 'Time after quench (us)',
-       ylabel = r'$\widetilde{\Gamma}_d / \widetilde{\Gamma}_{d,\mathrm{max}}$')
+       ylabel = r'$\widetilde{\Gamma}_d / \widetilde{\Gamma}_{d,\mathrm{max}}$',)
 
 for run in runs:
     # find data files
@@ -23,7 +33,8 @@ for run in runs:
     filename = datfiles[0].split(os.sep)[-1]
     print("Filename is " + filename)
     data = Data(filename, path=datfiles[0])
-
+    data.data['EF'] = EFs[runs.index(run)]
+    data.data['T'] = Ts[runs.index(run)]
     for key in defaults.keys():
         if key not in data.data.keys():
             data.data[key] = defaults[key]
@@ -36,6 +47,7 @@ for run in runs:
     ax.errorbar(df['hold time (us)'], 
                 df['norm_sig'], 
                 df['em_norm_sig'],
-                fmt='o', label=f'{filename[-7]} {df['EF'].values[0]/1000:.1f} kHz')
+                fmt='o', label=f'{filename[-7]} {int(df['T'].values[0])} nK?',
+                color=colors[runs.index(run)])
 
 ax.legend()
